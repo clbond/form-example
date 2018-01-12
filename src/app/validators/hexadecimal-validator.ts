f1807ed7fd686abe5d773b32804cda88c93a460d
Christopher Bond <cb@clbond.org>
2018-01-12 00:43:41 +0000

    Add asynchronous validation example

--- src/app/validators/hexadecimal-validator.ts
+++ src/app/validators/hexadecimal-validator.ts
@@ -3,7 +3,11 @@ import {Directive} from '@angular/core';
 import {
   NG_VALIDATORS,
   AbstractControl,
+  AsyncValidatorFn,
+  Validator
 } from '@angular/forms';
+
+import {Observable} from 'rxjs';
 
 @Directive({
   selector: '[hexadecimal][ngModel]',
@@ -11,18 +15,24 @@ import {
     { provide: NG_VALIDATORS, useExisting: HexadecimalValueValidator, multi: true }
   ]
 })
-export class HexadecimalValueValidator {
-  validate(control: AbstractControl): {[validator: string]: string} {
+export class HexadecimalValueValidator implements Validator {
+  validate(control: AbstractControl): Observable<{[validator: string]: string}> {
     const expression = /^([0-9a-fA-F]+)$/i;
     if (!control.value) { // the [required] validator will check presence, not us
-      return null;
+      return Observable.of(null);
     }
 
     const value = control.value.trim();
     if (expression.test(value)) {
-      return null;
+      return Observable.of(null);
     }
 
-    return {hexadecimal: 'Please enter a hexadecimal value (alphanumeric, 0-9 and A-F)'};
+    // Example of how to do asynchronous validation
+    return Observable.create(obs => {
+      setImmediate(() => {
+        obs.next({hexadecimal: 'Please enter a hexadecimal value (alphanumeric, 0-9 and A-F)'});
+        obs.complete();
+      });
+    });
   }
 }
